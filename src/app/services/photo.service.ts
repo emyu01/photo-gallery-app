d220ca926bec868f65604771ebbda0fd94a9cb33
Umar <madugu01@gmail.com>
2021-01-25 15:14:18 +0100

    [ADD] added platform specific logic to load saved images

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -38,18 +38,19 @@ constructor(platform: Platform) {
   const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
   this.photos = JSON.parse(photoList.value) || [];
 
-  // more to come...
-    // Display the photo by reading into base64 format
-  for (let photo of this.photos) {
-  // Read each saved photo's data from the Filesystem
-      const readFile = await Filesystem.readFile({
-      path: photo.filepath,
-      directory: FilesystemDirectory.Data
-  });
+  if (!this.platform.is('hybrid')) {
+      // Display the photo by reading into base64 format
+      for (let photo of this.photos) {
+        // Read each saved photo's data from the Filesystem
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+        });
 
-  // Web platform only: Load the photo as base64 data
-  photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
-}
+        // Web platform only: Load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
+    }
 }
 
 private async savePicture(cameraPhoto: CameraPhoto) {
